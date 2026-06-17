f220b64f169f1dcc3cab63918a6771f9712a6126
atul2323 <atulshahi82106@gmail.com>
2026-06-17 16:58:57 +0530

    Update model version in get_response function

--- google_studio.py
+++ google_studio.py
@@ -10,7 +10,7 @@ client = genai.Client(api_key=api_key)
 
 def get_response(prompt):
     response = client.models.generate_content(
-        model="gemini-2.5-flash",
+        model="gemini-3.5-flash",
         contents=prompt
     )
     return response.text
